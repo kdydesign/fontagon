a386ce893483ac8cc78341dfa70808f56e72fe33
kdydesign <kdydesign@gmail.com>
2020-04-27 18:56:21 +0900

    fix: correct error build clear

--- packages/cli/bin/fontagon.js
+++ packages/cli/bin/fontagon.js
@@ -1,24 +1,42 @@
+#!/usr/bin/env node
+
 const { program } = require('commander')
 const chalk = require('chalk')
+const leven = require('leven')
 
 const cleanArgs = require('../utils/cleanArgs')
-console.log('21321')
 
+// info
 program
   .version(`fontagon-cli ${require('../package').version}`)
   .usage('<command> [options]')
 
+// generate
 program
   .command('generate <svg-path>')
-  .description('build svg to create style sheet and font')
-  .option('-s --style <style>, dsadsa')
+  .description('Build svg to create style sheets and fonts (multiple path inputs as \',\')')
+  .option('-s, --style <style>', 'Type of css or css pre-processor to export')
+  .option('-f, --fontName <font-name>', 'Specify a font name and the default name for the font file')
+  .option('-d, --dist <dist>', 'Directory for generated font files')
+  .option('-b, --baseClass <base-class>', 'Stylesheet Default Class Name')
+  .option('-p, --classPrefix <class-prefix>', 'Prefix of icon class name')
   .action((svgPath, cmd) => {
     const options = cleanArgs(cmd)
 
     require('../lib/generate')(svgPath, options)
   })
 
-// add some useful info on help
+// unknown commands
+program
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp()
+    console.log('  ' + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    console.log()
+    suggestCommands(cmd)
+  })
+
+// --help
 program.on('--help', () => {
   console.log()
   console.log(`  Run ${chalk.cyan('fontagon <command> --help')} for detailed usage of given command.`)
@@ -31,4 +49,21 @@ program.parse(process.argv)
 
 if (!process.argv.slice(2).length) {
   program.outputHelp()
+}
+
+function suggestCommands (unknownCommand) {
+  const availableCommands = program.commands.map(cmd => cmd._name)
+
+  let suggestion
+
+  availableCommands.forEach((cmd) => {
+    const isBestMatch = leven(cmd, unknownCommand) < leven(suggestion || '', unknownCommand)
+    if (leven(cmd, unknownCommand) < 3 && isBestMatch) {
+      suggestion = cmd
+    }
+  })
+
+  if (suggestion) {
+    console.log('  ' + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
+  }
 }
--- packages/cli/lib/generate.js
+++ packages/cli/lib/generate.js
@@ -1,15 +1,61 @@
+/**
+ * command generate
+ *
+ * Dev.DY <kdydesign@gmail.com>
+ * issue: https://github.com/kdydesign/fontagon/issues
+ *
+ * MIT Lisence
+ */
+
 const Fontagon = require('fontagon')
 
-function generate (svgPath, options) {
+function generate (svgPath, opts) {
+  const options = assignOpts(opts)
+
   return Fontagon({
-    files: [svgPath],
+    files: separatedSvg(svgPath),
     ...options
   })
+}
+
+/**
+ * merge options
+ *
+ * @param opts
+ * @returns {{}}
+ */
+function assignOpts (opts) {
+  let options = {}
+
+  for (const key in opts) {
+    if (key === 'baseClass' || key === 'classPrefix') {
+      options.classOptions = Object.assign({}, options.classOptions, {
+        [key]: opts[key]
+      })
+    } else {
+      options = Object.assign(options, {
+        [key]: opts[key]
+      })
+    }
+  }
+
+  return options
+}
+
+/**
+ * svg path separate
+ *
+ * @param path
+ * @returns {unknown[]}
+ */
+function separatedSvg (path) {
+  return path.split(',').filter(svg => svg !== '')
 }
 
 module.exports = (...args) => {
   return generate(...args)
     .then((opts) => {
+      process.exit(1)
     })
     .catch((err) => {
       // stopSpinner(false) // do not persist
